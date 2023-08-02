46ebf277e3358bd80be4e38e0d2a1f330d5f74a7
merchan98 <37161019+merchan98@users.noreply.github.com>
2023-08-02 16:09:40 +0200

    Add files via upload

---
+++ PRACTICAS/PRACTICA 2/Ejercicios/prueba.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Prueba de PHP </title>
+    <head>
+    <body>
+        <p>Esto es una linea HTML. </p>
+        <?php
+            echo "<p> Y esto una linea de PHP.</p>";
+            phpinfo();
+        ?>
+    </body>
+<html>
